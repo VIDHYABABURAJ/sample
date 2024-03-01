f6982277a0a5b5334ff9fa4ff5c3d907091d9adb
VIDHYABABURAJ <vidhyababurajvidhya@gmail.com>
2024-03-01 11:26:29 +0530

    initial message

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp( MyApp());
+}
+
+class MyApp extends StatelessWidget {
+   MyApp({super.key});
+
+String a="hello world";
+int b=1;
+double c=1.55;
+
+
+
+
+
+
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Text('helfllllo $b '
+            ,style: TextStyle(fontSize: 30),),
+        ),
+      )
+    );
+  }
+}
+
